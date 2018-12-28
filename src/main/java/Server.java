6776dadd664067e136025c1b2a251ad48465df75
strus100 <danek130@gmail.com>
2018-12-28 18:00:50 +0100

    3 commit

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,64 +3,64 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.concurrent.CountDownLatch;
 
 public class Server {
 
-    public static int getPlayersNumber() {
-        return playersNumber;
-    }
-
-    public static int playersNumber = 0;
+    public static CountDownLatch latch = new CountDownLatch(1);
+    public static String value = " ";
 
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(3333);
         System.out.println("Oczekuję na połączenie");
 
         Socket socket1 = serverSocket.accept();
-       // Socket socket2 = serverSocket.accept();
-    //    Socket socket3 = serverSocket.accept();
-    //    Socket socket4 = serverSocket.accept();
-    //    Socket socket5 = serverSocket.accept();
-
+        Socket socket2 = serverSocket.accept();
+        Socket socket3 = serverSocket.accept();
+        //    Socket socket4 = serverSocket.accept();
+        //    Socket socket5 = serverSocket.accept();
 
         EchoThread thread1 = new EchoThread(socket1);
-//        EchoThread thread2 = new EchoThread(socket2);
-  //      EchoThread thread3 = new EchoThread(socket3);
-  //      EchoThread thread4 = new EchoThread(socket4);
-  //      EchoThread thread5 = new EchoThread(socket5);
+        EchoThread thread2 = new EchoThread(socket2);
+        EchoThread thread3 = new EchoThread(socket3);
+        //      EchoThread thread4 = new EchoThread(socket4);
+        //      EchoThread thread5 = new EchoThread(socket5);
 
 
         while (true){
+
             thread1.run();
-  //          thread2.run();
-    //        thread3.run();
-      //      thread4.run();
-         //   thread5.run();
+            EchoThread.clientValue += value;
+            thread2.run();
+            EchoThread.clientValue += value;
+            thread3.run();
+            EchoThread.clientValue += value;
+            //      thread4.run();
+            //   thread5.run();
         }
-
-
-        //TODO for zatrzymuj wątki i wznawiaj tylko gdy ID == i
-        }
+    }
 }
 
 
 class EchoThread extends Thread {
     protected Socket socket;
     public int guard = 0;
+    static String clientValue = "";
 
     public EchoThread(Socket clientSocket) {
         this.socket = clientSocket;
 
     }
 
-   synchronized public void run() {
+    synchronized public void run() {
+        int userGuard = 0;
+        boolean userGuardSwitch = true;
 
         try {
             ArrayList<String> list = new ArrayList<String>();
             StringTokenizer stringTokenizer;
             String recvfrom;
             String sendto;
-
 
             //Komunikacja od klienta
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(
@@ -71,57 +71,73 @@ class EchoThread extends Thread {
                     socket.getOutputStream());
 
             if(guard == 0) {
-                sendto = "POLACZONO" + '\n';
-                outToClient.writeBytes(sendto);
-                guard++;
+                outToClient.writeBytes("POLACZONO" + '\n');
             }
             else{
-                sendto = "TWOJ RUCH" + '\n';
-                outToClient.writeBytes(sendto);
 
+                outToClient.writeBytes(clientValue + "TWOJ RUCH" + '\n');
             }
-                    recvfrom = inFromClient.readLine();
-                        stringTokenizer = new StringTokenizer(recvfrom);
 
-                        while (stringTokenizer.hasMoreTokens()) {
-                            list.add(stringTokenizer.nextToken());
+            do {
+
+            recvfrom = inFromClient.readLine();
+            stringTokenizer = new StringTokenizer(recvfrom);
+
+            while (stringTokenizer.hasMoreTokens()) {
+                list.add(stringTokenizer.nextToken());
+            }
+
+
+                switch (list.get(0)) {
+                    case "LOGIN":
+                        System.out.println("Key: " + recvfrom);
+                        if(guard == 0){
+                            outToClient.writeBytes("START" + '\n');
+                            guard++;
                         }
+                        else {
+                            outToClient.writeBytes("ERROR" + '\n');
+                        }
+                       list.clear();
 
-                        switch (list.get(0)) {
-                            case "LOGIN":
-                                System.out.println("Key: " + recvfrom);
-                                sendto = "START" + '\n';
-                                outToClient.writeBytes(sendto);
-                                list.clear();
-                                break;
-                            case "PLANSZA":
-                                System.out.println("Key: " + recvfrom);
-                                sendto = "PLANSZA OK" + '\n';
-                                outToClient.writeBytes(sendto);
-                                list.clear();
-                                break;
-                            case "PASS":
-                                System.out.println("Key: " + recvfrom);
-                                sendto = "PASS OK" + '\n';
-                                outToClient.writeBytes(sendto);
-                                list.clear();
-                                break;
-                            case "ATAK":
-                                System.out.println("Key: " + recvfrom);
-                                sendto = "ATAK OK" + '\n';
-                                outToClient.writeBytes(sendto);
-                                list.clear();
-                                break;
-                            default:
-                                System.out.println("Key: " + recvfrom);
-                                sendto = "ERROR" + '\n';
-                                outToClient.writeBytes(sendto);
-                                list.clear();
-                                break;
+                        userGuardSwitch = false;
+                        break;
+                    case "PASS":
+                        System.out.println("Key: " + recvfrom);
+                        outToClient.writeBytes("PASS OK" + '\n');
+                        userGuardSwitch = false;
+                        list.clear();
+                        Server.value = "";
+                        Server.latch.countDown();
+
+                        break;
+                    case "ATAK":
+                        System.out.println("Key: " + recvfrom);
+                        outToClient.writeBytes("ATAK OK" + '\n');
+                        list.clear();
+
+                        sendto = "WYNIK DO WSZYSTKICH ";
+                        Server.value = sendto;
+                        Server.latch.countDown();
+
+                        outToClient.writeBytes("BY ZAKONCZYC KOLEJKE WPISZ PASS" + '\n');
+                        userGuardSwitch = true;
+                        break;
+                    default:
+                        System.out.println("Key: " + recvfrom);
+                        outToClient.writeBytes("ERROR" + '\n');
+                        list.clear();
+
+                        userGuard++;
+                        userGuardSwitch = false;
+                        break;
                 }
+                if (userGuard == 50)
+                    socket.close();
+           }while(userGuardSwitch);
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
-   }
+    }
 }
